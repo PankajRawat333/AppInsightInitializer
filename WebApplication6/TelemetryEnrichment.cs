1e1c79c22c4b70dd39131ceeffc587fe26fff3ef
Pankaj Rawat <pankajrawat.333@gmail.com>
2021-01-02 10:15:18 +0530

    Add files via upload

---
+++ WebApplication6/TelemetryEnrichment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ApplicationInsights.AspNetCore.TelemetryInitializers;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication6
+{
+    public class TelemetryEnrichment : TelemetryInitializerBase
+    {
+        public TelemetryEnrichment(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+        }
+
+        protected override void OnInitializeTelemetry(HttpContext platformContext, RequestTelemetry requestTelemetry, ITelemetry telemetry)
+        {
+            telemetry.Context.User.AuthenticatedUserId = "AuthId";
+            telemetry.Context.User.AccountId = "AccountId";
+            telemetry.Context.User.Id = "id-123";
+            telemetry.Context.Properties["Tenant"] = "RSI";
+        }
+    }
+}
